a5c59d1728f42e01fccadf54760f9ff894ff8b58
Mohit Nalavadi <mnalavadi@gmail.com>
2019-07-12 17:11:42 +0200

    toggle nav bar

--- js/vertical_tab.js
+++ js/vertical_tab.js
@@ -1,5 +1,4 @@
-// script to enable vertical nav bar
-
+// Vertical navigation bar
 function openSample(evt, sample) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -14,7 +13,40 @@ function openSample(evt, sample) {
     evt.currentTarget.className += " active";
 }
 
+
 // Get the element with id="defaultOpen" and click on it
 function clickTabOnLoad(){
     document.getElementById("defaultOpen").click();
+}
+
+
+// Toggle the vertical navigation bar, and extend the display
+function toggleVerticalNavTab() {
+    var verticalNavTab = document.getElementById("verticalNavTab");
+    if (verticalNavTab.style.display === "none") {
+        verticalNavTab.style.display = "block";
+        verticalNavTab.style.width = "20%";
+
+        var tabContentElements = document.getElementsByClassName('tabcontent');
+        for(var i=0; i<tabContentElements.length; i++) {
+            tabContentElements[i].style.width = "80%";
+        }
+        var tabRendererElements = document.getElementsByClassName('renderer');
+        for(var i=0; i<tabRendererElements.length; i++) {
+//            tabRendererElements[i].style.width = "75vw";
+        }
+    }
+    else {
+        verticalNavTab.style.display = "none";
+        verticalNavTab.style.width = "0%";
+
+        var tabContentElements = document.getElementsByClassName('tabcontent');
+        for(var i=0; i<tabContentElements.length; i++) {
+            tabContentElements[i].style.width = "100%";
+        }
+        var tabRendererElements = document.getElementsByClassName('renderer');
+        for(var i=0; i<tabRendererElements.length; i++) {
+//            tabRendererElements[i].style.width = "100vw";
+        }
+    }
 }
